fc7229865538b31b075347c92906efa324268a93
Dennis Muth <dennis.muth@magicline.de>
2018-02-12 08:08:52 +0100

    Modifies the way mockups are created

--- tests/flask/conftest.py
+++ tests/flask/conftest.py
@@ -2,8 +2,8 @@ import pytest
 
 from rpi433rc.app import app
 from rpi433rc.business.devices import StatefulDevice, CodeDevice, SystemDevice
-from rpi433rc.business.rc433 import RFDevice
-from rpi433rc.api import device_db
+import rpi433rc.business.rc433 as rc433
+import rpi433rc.api as api
 
 
 @pytest.yield_fixture(scope='session')
@@ -20,28 +20,28 @@ def flask_client(flask_app):
 
 @pytest.yield_fixture(scope='function')
 def mocked_rfdevice(mocker):
-    mocker.patch.object(RFDevice, 'enable_tx')
-    mocker.patch.object(RFDevice, 'tx_code')
-    mocker.patch.object(RFDevice, 'cleanup')
-    RFDevice.enable_tx.return_value = None
-    RFDevice.tx_code.return_value = True
-    RFDevice.cleanup.return_value = None
+    mocker.patch.object(rc433.RFDevice, 'enable_tx')
+    mocker.patch.object(rc433.RFDevice, 'tx_code')
+    mocker.patch.object(rc433.RFDevice, 'cleanup')
+    rc433.RFDevice.enable_tx.return_value = None
+    rc433.RFDevice.tx_code.return_value = True
+    rc433.RFDevice.cleanup.return_value = None
 
-    yield RFDevice
+    yield rc433.RFDevice
 
 
 @pytest.yield_fixture(scope='function')
 def mocked_device_db(mocker):
-    mocker.patch.object(device_db, 'list')
-    mocker.patch.object(device_db, 'lookup')
-    mocker.patch.object(device_db, 'switch')
+    mocker.patch.object(api.device_db, 'list')
+    mocker.patch.object(api.device_db, 'lookup')
+    mocker.patch.object(api.device_db, 'switch')
 
-    device_db.list.return_value = [
+    api.device_db.list.return_value = [
         StatefulDevice(device=CodeDevice('device1', code_on=12345, code_off=23456), state=False),
         StatefulDevice(device=CodeDevice('device2', code_on=12345, code_off=23456), state=True),
         StatefulDevice(device=SystemDevice('device3', system_code="00001", device_code=4), state=True),
     ]
-    device_db.lookup.return_value = StatefulDevice(device=CodeDevice('device1', code_on=12345, code_off=23456), state=False)
-    device_db.switch.return_value = None
+    api.device_db.lookup.return_value = StatefulDevice(device=CodeDevice('device1', code_on=12345, code_off=23456), state=False)
+    api.device_db.switch.return_value = None
 
-    yield device_db
+    yield api.device_db
