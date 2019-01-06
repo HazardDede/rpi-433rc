14cc7b5e642450862e1664a21c77f0af2e566c4c
Dennis Muth <dennis.muth@magicline.de>
2019-01-06 20:17:08 +0100

    Refactores mqtt publisher and publisher module

--- rpi433rc/business/publish.py
+++ rpi433rc/business/publish.py
@@ -1,9 +1,11 @@
+from abc import abstractmethod
+
 import attr
 from .util import LogMixin
-from ..config import MQTT_HOST, MQTT_PASSWORD, MQTT_PORT, MQTT_TOPIC, MQTT_USER
 
 
 def from_config():
+    from ..config import MQTT_HOST
     if MQTT_HOST is not None:
         return MQTTPublisher.from_config()
     return DummyPublisher.from_config()
@@ -12,9 +14,11 @@ def from_config():
 @attr.s
 class Publisher(LogMixin):
     @classmethod
+    @abstractmethod
     def from_config(cls):
         raise NotImplementedError()
 
+    @abstractmethod
     def publish(self, device_name, state):
         raise NotImplementedError()
 
@@ -34,11 +38,12 @@ class MQTTPublisher(Publisher):
     port = attr.ib(converter=int, default=1883)
     user = attr.ib(converter=str, default=None)
     password = attr.ib(converter=str, default=None)
-    topic = attr.ib(converter=str, default='rc433')
+    topic_pattern = attr.ib(converter=str, default='rc433/{device_name}')
 
     @classmethod
     def from_config(cls):
-        return cls(host=MQTT_HOST, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD, topic=MQTT_TOPIC)
+        from ..config import MQTT_HOST, MQTT_PASSWORD, MQTT_PORT, MQTT_TOPIC, MQTT_USER
+        return cls(host=MQTT_HOST, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD, topic_pattern=MQTT_TOPIC)
 
     def _publish(self, auth, qos, payload, retain, topic):
         import paho.mqtt.publish as publish
@@ -52,15 +57,21 @@ class MQTTPublisher(Publisher):
             auth=auth,
             qos=qos
         )
-        self.logger.debug("Published rc433 state '{payload}' on '{topic}' @ {self.host}:{self.port} with qos={qos}."
-                          .format(**locals()))
+        self.logger.info("Published rc433 state '{payload}' on '{topic}' @ {self.host}:{self.port} with qos={qos}."
+                         .format(**locals()))
 
     def _make_topic(self, device_name):
-        return self.topic + device_name if self.topic.endswith('/') else self.topic + '/' + device_name
+        return self.topic_pattern.format(device_name=device_name)
 
     def publish(self, device_name, state):
         real_topic = self._make_topic(device_name)
         auth = None
         if self.user is not None:
             auth = dict(username=self.user, password=self.password)
-        self._publish(auth, 1, 'on' if state else 'off', True, real_topic)
+        try:
+            self._publish(auth, qos=0, payload='on' if state else 'off', retain=True, topic=real_topic)
+        except Exception:
+            import traceback
+            error = traceback.format_exc()
+            self.logger.error("Error when publishing state '{state}' for device '{device_name}' on mqtt:\n"
+                              "{error}".format(**locals()))
